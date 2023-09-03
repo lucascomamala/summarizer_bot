b319d882c4f81d8a541ff3623d089cd4dd988b8f
Lucas <lucascomamala@gmail.com>
2023-09-03 07:00:18 -0600

    article endpoint

--- src/services/article.js
+++ src/services/article.js
@@ -2,9 +2,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("X-rapidapi-key", import.meta.env.VITE_RAPID_API_ARTICLE_KEY);
+      headers.set("X-rapidapi-host", "article-extractor-and-summarizer.p.rapidapi.com");
+      return headers;
+    }
+  }),
   endpoints: (builder) => ({
     getSummary: builder.query({
-      query: (params) => `test`
+      query: (params) => `/sumamrize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
     })
   }),
 })
+
+export const { useLazyGetSummaryQuery } = articleApi
